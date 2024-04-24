15936f19a9b2f94f1de8261ae9d2db92782d8629
Nathan Ha <nathan_ha@outlook.com>
2024-04-24 16:25:32 -0700

    fixed weird hacky solution lab 4 pt 2

--- lab4/exercise2/src/nha023_lab4_part2.cpp
+++ lab4/exercise2/src/nha023_lab4_part2.cpp
@@ -6,7 +6,7 @@
 
  *         Exercise Description: Moving joystick in specific pattern activates motor
 
- *        
+ *
 
  *         I acknowledge all content contained herein, excluding template or example code, is my own original work.
 
@@ -16,131 +16,127 @@
 
  */
 
-
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "../lib/timerISR.h"
 #include "../lib/serialATmega.h"
 
-enum states {INIT, NEUTRAL, UP, DOWN, LEFT, RIGHT, PRESS, SUCCESS} state;
+enum states
+{
+  INIT,
+  NEUTRAL,
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+  PRESS,
+  SUCCESS
+} state;
 
-
-unsigned char SetBit(unsigned char number, unsigned char kth_bit, unsigned char bit) {
-   return (bit ?  (number | (0x01 << kth_bit))  :  (number & ~(0x01 << kth_bit)) );
-              //   Set bit to 1           Set bit to 0
+unsigned char SetBit(unsigned char number, unsigned char kth_bit, unsigned char bit)
+{
+  return (bit ? (number | (0x01 << kth_bit)) : (number & ~(0x01 << kth_bit)));
+  //   Set bit to 1           Set bit to 0
 }
 
-unsigned char GetBit(unsigned char number, unsigned char kth_bit) {
-   return ((number & (0x01 << kth_bit)) != 0);
+unsigned char GetBit(unsigned char number, unsigned char kth_bit)
+{
+  return ((number & (0x01 << kth_bit)) != 0);
 }
 
-void ADC_init() {
-  ADMUX = (1<<REFS0);
-	ADCSRA|= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
-	// ADEN: setting this bit enables analog-to-digital conversion.
-	// ADSC: setting this bit starts the first conversion.
-	// ADATE: setting this bit enables auto-triggering. Since we are
-	//        in Free Running Mode, a new conversion will trigger whenever
-	//        the previous conversion completes.
+void ADC_init()
+{
+  ADMUX = (1 << REFS0);
+  ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  // ADEN: setting this bit enables analog-to-digital conversion.
+  // ADSC: setting this bit starts the first conversion.
+  // ADATE: setting this bit enables auto-triggering. Since we are
+  //        in Free Running Mode, a new conversion will trigger whenever
+  //        the previous conversion completes.
 }
 
-
-
-unsigned int ADC_read(unsigned char chnl){
+unsigned int ADC_read(unsigned char chnl)
+{
   uint8_t low, high;
 
-  ADMUX  = (ADMUX & 0xF8) | (chnl & 7);
-  ADCSRA |= 1 << ADSC ;
-  while((ADCSRA >> ADSC)&0x01){}
+  ADMUX = (ADMUX & 0xF8) | (chnl & 7);
+  ADCSRA |= 1 << ADSC;
+  while ((ADCSRA >> ADSC) & 0x01)
+  {
+  }
 
+  low = ADCL;
+  high = ADCH;
 
-	low  = ADCL;
-	high = ADCH;
-
-	return ((high << 8) | low) ;
+  return ((high << 8) | low);
 }
 
-
-int nums[16] = {0b1111110, 0b0110000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1111011, 0b1110111, 0b0011111, 0b1001110, 0b0111101, 0b1001111, 0b1000111 }; 
+int nums[16] = {0b1111110, 0b0110000, 0b1101101, 0b1111001, 0b0110011, 0b1011011, 0b1011111, 0b1110000, 0b1111111, 0b1111011, 0b1110111, 0b0011111, 0b1001110, 0b0111101, 0b1001111, 0b1000111};
 // a  b  c  d  e  f  g
-void outNum(int num){
-	PORTD = nums[num] << 1; //assigns bits 1-7 of nums(a-f) to pins 2-7 of port d
-  PORTB = SetBit(PORTB, 0 ,nums[num]&0x01); // assigns bit 0 of nums(g) to pin 0 of port b
+void outNum(int num)
+{
+  PORTD = nums[num] << 1;                     // assigns bits 1-7 of nums(a-f) to pins 2-7 of port d
+  PORTB = SetBit(PORTB, 0, nums[num] & 0x01); // assigns bit 0 of nums(g) to pin 0 of port b
 }
 
-//directions[] and outDir() will be neeeded for ex 2 and 3
-// a  b  c  d  e  f  g
-void outDir(enum states dir){
-  switch (dir) {
-    case UP:
-      PORTD = 0b0111110 << 1;
-      PORTB = SetBit(PORTB, 0 ,0b0111110 & 0x01);
-      break;
-    case DOWN:
-      PORTD = 0b0111101 << 1;
-      PORTB = SetBit(PORTB, 0 ,0b0111101 & 0x01);
-      break;
-    case LEFT:
-      PORTD = 0b0001110 << 1;
-      PORTB = SetBit(PORTB, 0, 0b0001110 & 0x01);
-      break;
-    case RIGHT:
-      PORTD = 0b0000101 << 1;
-      PORTB = SetBit(PORTB, 0, 0b0000101 & 0x01);
-      break;
-    default:
-      break;
+// directions[] and outDir() will be neeeded for ex 2 and 3
+//  a  b  c  d  e  f  g
+void outDir(enum states dir)
+{
+  switch (dir)
+  {
+  case UP:
+    PORTD = 0b0111110 << 1;
+    PORTB = SetBit(PORTB, 0, 0b0111110 & 0x01);
+    break;
+  case DOWN:
+    PORTD = 0b0111101 << 1;
+    PORTB = SetBit(PORTB, 0, 0b0111101 & 0x01);
+    break;
+  case LEFT:
+    PORTD = 0b0001110 << 1;
+    PORTB = SetBit(PORTB, 0, 0b0001110 & 0x01);
+    break;
+  case RIGHT:
+    PORTD = 0b0000101 << 1;
+    PORTB = SetBit(PORTB, 0, 0b0000101 & 0x01);
+    break;
+  default:
+    break;
   }
 }
 
-int phases[8] = {0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001}; //8 phases of the stepper motor step
+int phases[8] = {0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001}; // 8 phases of the stepper motor step
 
 enum states password[4] = {UP, DOWN, LEFT, RIGHT};
 enum states moves[4] = {NEUTRAL, NEUTRAL, NEUTRAL, NEUTRAL};
 
 char turn_num = 0;
 
-
-bool password_is_correct(void) {
-  for (int i = 0; i < 4; i++) {
-    if (password[i] != moves[i]) return false;
+bool password_is_correct(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    if (password[i] != moves[i])
+      return false;
   }
   return true;
 }
 
-void resetMoves(void) {
-  for (int i = 0; i < 4; i++) 
+void resetMoves(void)
+{
+  for (int i = 0; i < 4; i++)
     moves[i] = NEUTRAL;
-}
-
-// spins the motor. direction spins one way for direction == 0, else it spins the other way
-void spin_motor(int ticks, char direction) {
-  int i = 0;
-  for (int t = 0; t < ticks; t++) {
-      if(direction){ //button not pressed
-         PORTB = (PORTB & 0x03) | phases[i] << 2; //& first to reset pins 2-5 but not 0-1 then | with phase shifted left 2 to assign the right value to pins 2-5
-         i++; //increment to next phase
-         if(i > 7){ //if all phases are completed, restart
-            i = 0;
-         }
-     }else{
-         PORTB = (PORTB & 0x03) | phases[i] << 2;
-         i--;
-         if(i < 0){
-             i = 8;
-         }
-    }
-    while (!TimerFlag){}  // Wait for SM period
-    TimerFlag = 0;
-  }
 }
 
 char motor_is_done = 0;
 char locked_status = 1;
+char phase_index = 0;
+int ticks = 0;
 
-void Tick() {
+void Tick()
+{
   const int stick_x = ADC_read(0);
   const int stick_y = ADC_read(1);
 
@@ -152,145 +148,188 @@ void Tick() {
   const char stick_neutral = !stick_up && !stick_down && !stick_left && !stick_right;
 
   // State Transistions
-  //TODO: complete transitions 
-  switch(state) {
+  // TODO: complete transitions
+  switch (state)
+  {
 
-    case INIT:
+  case INIT:
+    state = NEUTRAL;
+    break;
+  case NEUTRAL:
+    if (turn_num >= 4 && password_is_correct())
+    {
+      state = SUCCESS;
+    }
+    else if (stick_up)
+    {
+      state = UP;
+      moves[turn_num] = UP;
+    }
+    else if (stick_down)
+    {
+      state = DOWN;
+      moves[turn_num] = DOWN;
+    }
+    else if (stick_left)
+    {
+      state = LEFT;
+      moves[turn_num] = LEFT;
+    }
+    else if (stick_right)
+    {
+      state = RIGHT;
+      moves[turn_num] = RIGHT;
+    }
+    break;
+  case UP:
+    if (stick_neutral)
+    {
       state = NEUTRAL;
-      break;
-    case NEUTRAL:
-      if (turn_num >= 4 && password_is_correct()) {
-        state = SUCCESS;
-      }
-      else if (stick_up) {
-        state = UP;
-        moves[turn_num] = UP;
-      }
-      else if (stick_down) {
-        state = DOWN;
-        moves[turn_num] = DOWN;
-      }
-      else if (stick_left) {
-        state = LEFT;
-        moves[turn_num] = LEFT;
-      }
-      else if (stick_right) {
-        state = RIGHT;
-        moves[turn_num] = RIGHT;
-      }
-      break;
-    case UP:
-      if (stick_neutral) {
-        state = NEUTRAL;
-        turn_num++;
-      }
-      else {
-        state = UP;
-      }
-      break;
-    case DOWN:
-      if (stick_neutral) {
-        state = NEUTRAL;
-        turn_num++;
-      }
-      else {
-        state = DOWN;
-      }
-      break;
-    case LEFT:
-      if (stick_neutral) {
-        state = NEUTRAL;
-        turn_num++;
-      }
-      else {
-        state = LEFT;
-      }
-      break;
-    case RIGHT:
-      if (stick_neutral) {
-        state = NEUTRAL;
-        turn_num++;
-      }
-      else {
-        state = RIGHT;
-      }
-      break;
-    case SUCCESS:
-      if (motor_is_done) {
-        state = NEUTRAL;
-        turn_num = 0;
-        motor_is_done = 0;
-        resetMoves();
-      }
-      else {
-        state = SUCCESS;
-      }
-      break;
-    default:
-      state = INIT;
-      break;
-
+      turn_num++;
+    }
+    else
+    {
+      state = UP;
+    }
+    break;
+  case DOWN:
+    if (stick_neutral)
+    {
+      state = NEUTRAL;
+      turn_num++;
+    }
+    else
+    {
+      state = DOWN;
+    }
+    break;
+  case LEFT:
+    if (stick_neutral)
+    {
+      state = NEUTRAL;
+      turn_num++;
+    }
+    else
+    {
+      state = LEFT;
+    }
+    break;
+  case RIGHT:
+    if (stick_neutral)
+    {
+      state = NEUTRAL;
+      turn_num++;
+    }
+    else
+    {
+      state = RIGHT;
+    }
+    break;
+  case SUCCESS:
+    if (motor_is_done)
+    {
+      state = NEUTRAL;
+      turn_num = 0;
+      motor_is_done = 0;
+      resetMoves();
+    }
+    else
+    {
+      state = SUCCESS;
+    }
+    break;
+  default:
+    state = INIT;
+    break;
   }
 
   // State Actions
-  switch(state) {
-    case INIT:
-      break;
-    case NEUTRAL:
-      if (turn_num >= 4 && !password_is_correct()) turn_num = 0;
-      if (locked_status) PORTC = SetBit(PORTC, 5, 1);
-      outNum(turn_num);
-      break;
-    case UP:
-      outDir(UP);
-      break;
-    case DOWN:
-      outDir(DOWN);
-      break;
-    case LEFT:
-      outDir(LEFT);
-      break;
-    case RIGHT:
-      outDir(RIGHT);
-      break;
-    case SUCCESS:
-      outNum(4);
-      spin_motor(2000, locked_status);
+  switch (state)
+  {
+  case INIT:
+    break;
+  case NEUTRAL:
+    if (turn_num >= 4 && !password_is_correct())
+      turn_num = 0;
+    if (locked_status)
+      PORTC = SetBit(PORTC, 5, 1);
+    outNum(turn_num);
+    break;
+  case UP:
+    outDir(UP);
+    break;
+  case DOWN:
+    outDir(DOWN);
+    break;
+  case LEFT:
+    outDir(LEFT);
+    break;
+  case RIGHT:
+    outDir(RIGHT);
+    break;
+  case SUCCESS:
+    outNum(4);
+    // spin motor
+    if (!motor_is_done)
+    {
+      if (locked_status)
+      {                                                    
+        PORTB = (PORTB & 0x03) | phases[phase_index] << 2; //& first to reset pins 2-5 but not 0-1 then | with phase shifted left 2 to assign the right value to pins 2-5
+        phase_index++;                                     
+        if (phase_index > 7)
+        { // if all phases are completed, restart
+          phase_index = 0;
+        }
+      }
+      else
+      {
+        PORTB = (PORTB & 0x03) | phases[phase_index] << 2;
+        phase_index--;
+        if (phase_index < 0)
+        {
+          phase_index = 8;
+        }
+      }
+    }
+    ticks++;
+    if (ticks >= 1000) {
       motor_is_done = 1;
+      ticks = 0;
       locked_status = !locked_status;
-      if (!locked_status) PORTC = SetBit(PORTC, 5, 0);
-      break;
-    default:
-      break;
-
+      if (!locked_status)
+        PORTC = SetBit(PORTC, 5, 0);
+    }
+    break;
+  default:
+    break;
   }
-
 }
-
-
 
 int main(void)
 {
-  DDRB = 0xFF; PORTB = 0x00; // init port B outputs
-  DDRD = 0xFF; PORTD = 0x00; // init port D outputs
+  DDRB = 0xFF;
+  PORTB = 0x00; // init port B outputs
+  DDRD = 0xFF;
+  PORTD = 0x00; // init port D outputs
 
-
-  ADC_init();//initializes the analog to digital converter
+  ADC_init(); // initializes the analog to digital converter
   serial_init(9600);
 
-	
   state = INIT;
-  DDRC = 0x00; PORTC = 0xFF; // port c input
-  DDRC = SetBit(DDRC, 5, 1); // a5 output
+  DDRC = 0x00;
+  PORTC = 0xFF;                // port c input
+  DDRC = SetBit(DDRC, 5, 1);   // a5 output
   PORTC = SetBit(PORTC, 5, 0); // a5 output
-  TimerSet(1); //period of 1 ms. good period for the stepper mottor
+  TimerSet(1);                 // period of 1 ms. good period for the stepper mottor
   TimerOn();
-    while (1)
+  while (1)
+  {
+    Tick(); // Execute one synchSM tick
+    while (!TimerFlag)
     {
-		  Tick();      // Execute one synchSM tick
-      while (!TimerFlag){}  // Wait for SM period
-      TimerFlag = 0;        // Lower flag
-     }
+    }              // Wait for SM period
+    TimerFlag = 0; // Lower flag
+  }
 
-    return 0;
+  return 0;
 }
