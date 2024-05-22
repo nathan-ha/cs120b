d5bb55810d1f9a6e2f9bedbb984721024760fc28
Nathan Ha <nathanha562@gmail.com>
2024-05-22 00:27:11 -0700

    sped up display a bit and added lil animation

--- spi test/lib/spiAVR.h
+++ spi test/lib/spiAVR.h
@@ -24,7 +24,7 @@
 // If SS is on a different port, make sure to change the init to take that into account.
 void SPI_INIT() {
   DDRB |= (1 << PIN_MOSI) | (1 << PIN_SCK) | (1 << PIN_SS) | (1 << PIN_A0) | (1 << PIN_RST);
-  SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);  // initialize SPI coomunication
+  SPCR |= (1 << SPE) | (1 << MSTR); // initialize SPI coomunication
 }
 
 void SPI_SEND(char data) {
@@ -41,7 +41,7 @@ void SPI_SEND(char data) {
 // ****************************************************************************************************
 
 // Function to send command to display
-void TFT_SEND_COMMAND(uint8_t command) {
+void TFT_SEND_COMMAND(char command) {
   PORTB &= ~(1 << PIN_A0);  // set A0 (DC) low for command mode
   PORTB &= ~(1 << PIN_SS);  // select the tft display
   SPI_SEND(command);        // send command
@@ -49,7 +49,7 @@ void TFT_SEND_COMMAND(uint8_t command) {
 }
 
 // Function to send data to display
-void TFT_SEND_DATA(uint8_t data) {
+void TFT_SEND_DATA(char data) {
   PORTB |= (1 << PIN_A0);   // set A0 (DC) high for data mode
   PORTB &= ~(1 << PIN_SS);  // select the tft display
   SPI_SEND(data);           // send data
@@ -58,7 +58,6 @@ void TFT_SEND_DATA(uint8_t data) {
 
 // clears the display memory faster than writing one pixel at a time
 // same as the init function
-// note: could also use this logic to quickly make a rectangle
 void TFT_FLUSH() {
   TFT_SEND_COMMAND(0x2A);  // Set column address
   TFT_SEND_DATA(0x00);     // start column high byte
@@ -73,13 +72,38 @@ void TFT_FLUSH() {
   TFT_SEND_DATA(127);      // end row low byte
 
   TFT_SEND_COMMAND(0x2C);  // Memory write
-  for (short i = 0; i < 128 * 200; i++) {
+  for (short i = 0; i < 128 * 128; i++) {
     TFT_SEND_DATA(0x00);  // Write black color data to clear the screen
+    TFT_SEND_DATA(0x00);
+  }
+}
+
+// draws rectangles faster than a nested for loop
+// input is the two corners of the rectangle
+// same logic as flush
+void TFT_DRAW_RECTANGLE(char x1, char y1, char x2, char y2, short color) {
+  TFT_SEND_COMMAND(0x2A);  // Set column address
+  TFT_SEND_DATA(0x00);     // start column high byte
+  TFT_SEND_DATA(x1);       // start column low byte
+  TFT_SEND_DATA(0x00);     // end column high byte
+  TFT_SEND_DATA(x2);       // end column low byte
+
+  TFT_SEND_COMMAND(0x2B);  // Set row address
+  TFT_SEND_DATA(0x00);     // start row high byte
+  TFT_SEND_DATA(y1);       // start row low byte
+  TFT_SEND_DATA(0x00);     // end row high byte
+  TFT_SEND_DATA(y2);       // end row low byte
+
+  TFT_SEND_COMMAND(0x2C);  // Memory write
+  for (short i = 0; i < (x2 - x1) * (y2 - y1); i++) {
+    TFT_SEND_DATA(color >> 4);  // honestly don't know why i have to shift it like this
+    TFT_SEND_DATA(color << 4);  // datasheet doesn't mention it, but it's the only way it works
   }
 }
 
 // Function to initialize ST7735
 void TFT_INIT() {
+  SPI_INIT();
   // Reset the display
   // honestly not sure if we need to reset the display but it can't hurt
   PORTB &= ~(1 << PIN_RST);  // Reset low
@@ -91,7 +115,7 @@ void TFT_INIT() {
   TFT_SEND_COMMAND(0x01);  // Software reset
   _delay_ms(10);
 
-  TFT_SEND_COMMAND(0x11);  // Sleep out // don't know if this device ever actually goes to sleep
+  TFT_SEND_COMMAND(0x11);  // wake up from slep
   _delay_ms(10);
 
   TFT_SEND_COMMAND(0x3A);  // pixel format
@@ -99,16 +123,15 @@ void TFT_INIT() {
 
   TFT_FLUSH();  // erase leftover memory
 
-  // Set default rotation
   TFT_SEND_COMMAND(0x36);    // Memory Access Control
-  TFT_SEND_DATA(0b0101010);  // MY = 0, MX (mirror x) = 1, MV = 1, ML = 0, RGB (instead of bgr) = 1, MH = 0
+  TFT_SEND_DATA(0b01001000);  // MY = 0, MX (mirror x) = 1, MV = 1, ML = 0, RGB (instead of bgr) = 1, MH = 0
 
   // Set display on
   TFT_SEND_COMMAND(0x29);  // Display on
 }
 
 // draws pixels at the x y coords
-void TFT_DRAW_PIXEL(uint8_t x, uint8_t y, uint16_t color) {
+void TFT_DRAW_PIXEL(char x, char y, short color) {
   TFT_SEND_COMMAND(0x2A);  // Set column address
   TFT_SEND_DATA(0x00);     // start column high byte
   TFT_SEND_DATA(x);        // start column low byte
--- spi test/src/main.cpp
+++ spi test/src/main.cpp
@@ -13,6 +13,14 @@ int main() {
   //   }
   // }
 
+  // TFT_DRAW_RECTANGLE(64, 64, 127, 100, 0xFFF);
+
+  short x1 = 10, y1 = 50, dir_flag = 0, dir_flagy = 0;
+
+  // TFT_DRAW_RECTANGLE(0, 0, 127, 127, 0xFFF);
+
+  // TFT_SEND_COMMAND(0x12);  // partial mode
+
   while (1) {
     // for (int i = 0; i < 64; i++) {
     //   for (int j = 0; j < 64; j++) {
@@ -24,11 +32,41 @@ int main() {
     //   TFT_DRAW_PIXEL(i, i * i, 0xFF);
     // }
 
-    for (short i = 64; i < 80; i++) {
-      for (short j = 64; j < 80; j++) {
-        TFT_DRAW_PIXEL(i, j, 0xfff);
-      }
-    }
+    // for (short i = 64; i < 80; i++) {
+    //   for (short j = 64; j < 80; j++) {
+    //     TFT_DRAW_PIXEL(i, j, 0xfff);
+    //   }
+    // }
+
     // TFT_FLUSH();
+    // for (int i = 0; i < 128; i++) {
+    //   TFT_DRAW_PIXEL(i, i*i, 0x0F0);
+    // }
+
+    // TFT_DRAW_PIXEL(127, 127, 0xFFF);
+    TFT_DRAW_RECTANGLE(x1, y1, x1 + 10, y1 + 10, 0xFFF);
+    _delay_ms(30);
+    TFT_DRAW_RECTANGLE(x1, y1, x1 + 10, y1 + 10, 0);
+
+    if (x1 >= 120) {
+      dir_flag = 0;
+    } else if (x1 <= 0) {
+      dir_flag = 1;
+    }
+    if (y1 >= 120) {
+      dir_flagy = 0;
+    } else if (y1 <= 0) {
+      dir_flagy = 1;
+    }
+    if (dir_flag) {
+      x1 += 2;
+    } else {
+      x1 -= 2;
+    }
+    if (dir_flagy) {
+      y1 += 2;
+    } else {
+      y1 -= 2;
+    }
   }
 }
